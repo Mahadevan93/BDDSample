57c7181ee598375aced9109a6742cbe8b9ab6389
MAHADEV <MAHADEV@DESKTOP-GQBKCBI>
2022-05-26 12:18:01 +0530

    Made some changes in CRMStepDefs

--- src/test/java/stepDefs/CRMStepDefs.java
+++ src/test/java/stepDefs/CRMStepDefs.java
@@ -14,6 +14,7 @@ public class CRMStepDefs {
 	@Before (value = "@RegressionTest", order=2)
 	public void initializeBrowser() {
 		System.out.println("Initialize the Browser");
+		System.out.println("Browser Initialized");
 		
 	}
 	
